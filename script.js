677a4259b45730491091fdb8055de8241fa59ef3
Neha Kumari <neha.kumari80923@gmail.com>
2025-08-10 10:03:41 +0530

    add files

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-document.onkeydown = function(e){
-    console.log("key code is: " , )
-}
+
+const keyMap = {
+    "ArrowUp": 38,
+    "ArrowDown": 40,
+    "ArrowLeft": 37,
+    "ArrowRight": 39,
+    "a": 65, "A": 65,
+    "d": 68, "D": 68,
+    "w": 87, "W": 87,
+    "s": 83, "S": 83,
+    " ": 32, // Space
+    "Enter": 13
+};
+
+document.onkeydown = function(e) {
+    let keyName = e.key; 
+    let keyCode = keyMap[keyName] || 0; 
+    console.log(`Key pressed: ${keyName}`);
+    console.log(`Key code is: ${keyCode}`);
+};
+
+
